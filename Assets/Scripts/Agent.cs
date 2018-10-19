89978bae058773538798b848791d3a7ded10218d
avokadoen <akselhallajs@gmail.com>
2018-10-19 19:42:24 +0200

    Pair programming agent

---
+++ Assets/Scripts/Agent.cs
@@ -0,0 +1,91 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum pace {walking = 1, jogging = 3, running = 5, sprinting = 10}
+
+[RequireComponent(typeof(PhysicalObject), typeof(Rigidbody))]
+public class Agent : MonoBehaviour {
+
+    Rigidbody rb;
+
+    PhysicalObject physicalObject;
+    PhysicalObject heldObject;
+
+    public float hunger;
+    public float sleepyness;
+    public float tempreture;
+    public float maxMoveSpeed;
+    public float stamina;
+    public float perception;
+    public pace currentPace;
+
+    // TODO: move to function stack
+    public GameObject moveTowardsTarget;
+
+
+    private float getPace() { return (float)currentPace / 10f; }
+
+    // Use this for initialization
+    void Start () {
+        physicalObject = GetComponent<PhysicalObject>();
+        rb = GetComponent<Rigidbody>();
+
+        // TODO: Agent testing values: remove
+        hunger          = 0f;
+        sleepyness      = 0f;
+        stamina         = 1f;
+        maxMoveSpeed    = 5f;
+        tempreture      = 38f;
+        perception      = 10f;
+    }
+
+    private void Update()
+    {
+        moveTowards();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate () {
+		// assertState()
+        // takeAction()
+	}
+
+    // Actions
+
+    private void moveTowards()
+    {
+        Vector3 lookAt = moveTowardsTarget.transform.position;
+        lookAt.y = transform.position.y;
+        transform.LookAt(lookAt);
+        if (Physics.Raycast(transform.position, transform.forward, 1f)) { return; } // perception * (1 - sleepyness))
+
+        else if (moveTowardsTarget.transform.position.x == transform.position.x && 
+                moveTowardsTarget.transform.position.z == transform.position.z) return;
+
+        float staminaToBeUsed = getPace() * Time.deltaTime;
+        if (isMovingFast() && stamina < staminaToBeUsed)
+        {
+            currentPace = pace.jogging;
+        }
+
+
+        rb.velocity = maxMoveSpeed * getPace() * transform.forward;
+        
+        if(isMovingFast())
+        {
+            stamina -= staminaToBeUsed;
+        }
+        
+    }
+
+    private void updateStates()
+    {
+
+    }
+
+    private bool isMovingFast()
+    {
+        return (currentPace == pace.sprinting || currentPace == pace.running);
+    }
+}
---
+++ Assets/Scripts/PhysicalObject.cs
@@ -0,0 +1,98 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.EventSystems;
+
+
+[RequireComponent(typeof(Rigidbody), typeof(Collider))]
+public class PhysicalObject : MonoBehaviour
+{
+    const float DAMAGE_TRESHOLD = 7.5f;
+
+    Rigidbody body;
+    Collider objCollider;
+
+    public float durability; //How much this object can take before being broken, -1 is unbreakable;
+    [SerializeField] private float density; //How dense an object is determines how much damage it applies to other objects
+
+    private Vector3 velocity;
+    private Vector3 torque;
+
+    private bool isPickedUp;
+    Transform handTransform; //TODO: we might need to use this for velocity later on if 
+
+
+    [System.Serializable] public class ColliderEvent : UnityEvent<Collider> { };
+    [System.Serializable] public class HandEvent : UnityEvent<Transform> { };
+
+    public UnityEvent
+        onDestroy,
+        onHolding;
+    public ColliderEvent
+        onHit;
+    public HandEvent
+        onPickup,
+        onDrop;
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (isPickedUp && (collision.gameObject.tag == "Hand"))
+            return; //ignore this collision
+
+        {
+            float otherDensity = collision.gameObject.GetComponent<PhysicalObject>()?.density ?? 0.5f;
+            if (otherDensity <= 0) return;
+            float potentialDamage = otherDensity * collision.relativeVelocity.magnitude;
+            durability -= (potentialDamage > DAMAGE_TRESHOLD * density) ? potentialDamage : 0;
+            if (durability <= 0) Destroy(gameObject);
+        }
+        //onHit.Invoke(collision.collider);
+    }
+
+    /// <summary>
+    /// Called when picking up this item
+    /// </summary>
+    public void OnPickUp(Transform pickedUpByHand)
+    {
+        //internal stuff
+
+        //NOTE: REMEMBER TO USE RIGIDBODY.MovePosition instead of changing transform.position to avoid weird physics, or lack thereof
+        body.isKinematic = true;
+
+
+        //external stuff
+        onPickup.Invoke(pickedUpByHand);
+    }
+
+    /// <summary>
+    /// Called every fixedupdate frame when holding this
+    /// </summary>
+    public void OnHolding(Transform heldByHand)
+    {
+        //internal
+
+        //maybe calculate velocity or not?;
+
+        //external stuff
+        onHolding.Invoke();
+    }
+
+    public void OnDrop(Transform droppedByHand)
+    {
+        //internal stuff
+
+        //NOTE: REMEMBER TO USE RIGIDBODY.MovePosition instead of changing transform.position to avoid weird physics, or lack thereof
+        body.isKinematic = false;
+
+        //body.velocity = droppedByHand.GetTrackedObjectVelocity();
+
+        //external stuff
+        onDrop.Invoke(droppedByHand);
+    }
+
+    private void OnDestroy()
+    {
+        onDestroy.Invoke();
+    }
+}

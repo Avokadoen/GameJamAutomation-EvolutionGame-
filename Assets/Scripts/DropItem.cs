72efc675a5a51780f473484d55dfefe04ef27411
akerholten <nikolai_aakk@hotmail.com>
2018-10-20 15:16:32 +0200

    Started work on map generation

--- Assets/Scripts/DropItem.cs
+++ Assets/Scripts/DropItem.cs
@@ -5,6 +5,6 @@ using UnityEngine;
 public class DropItem : MonoBehaviour {
 	public void Drop(GameObject toInstantiate)
     {
-        Instantiate<GameObject>(toInstantiate, new Vector3(transform.position.x, transform.position.y + 0.5f, transform.position.z), Quaternion.Euler(Vector3.up));
+        Instantiate<GameObject>(toInstantiate, new Vector3(transform.position.x, transform.position.y + 0.5f, transform.position.z), Quaternion.identity);
     }
 }
---
+++ Assets/Scripts/WorldGenerator.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Random = System.Random;
+
+public class WorldGenerator : MonoBehaviour {
+
+    [SerializeField] public string testSeed = "ItJustWorks";
+    [Tooltip("Biome grid in biomes x biomes size")]
+    [SerializeField] private int biomesGrid;
+    [Tooltip("Size of each biome in tiles x tiles size")]
+    [SerializeField] private int biomeSize;
+    [SerializeField] private int biomeOddsFavour;
+
+    public GameObject[] groundTiles;
+    public GameObject[] objectsToSpawn;
+    public GameObject[] animalsToSpawn;
+
+
+    // Use this for initialization
+    void Start () {
+        Random rand = new Random(testSeed.GetHashCode());
+        GenerateGround(rand);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void GenerateGround(Random rand)
+    {
+        int amountOfGroundTiles = groundTiles.Length;
+        for (int biomeY = 0; biomeY < biomesGrid; biomeY++)
+        {
+            for(int biomeX = 0; biomeX < biomesGrid; biomeX++)
+            {
+                // Pick biome-type
+                int biomeType = rand.Next(amountOfGroundTiles);
+                // Set odds
+                // Generate random biome
+
+
+                for (int y = 0; y < biomeSize; y++)
+                {
+                    for (int x = 0; x < biomeSize; x++)
+                    {
+                        int spawnRoll = rand.Next(biomeOddsFavour);
+                        Vector3 spawnPosition = new Vector3(biomeX * biomeSize + x, 0, biomeY * biomeSize + y);
+                        if(spawnRoll % biomeOddsFavour != 0)
+                        {
+                            GameObject.Instantiate<GameObject>(groundTiles[biomeType], spawnPosition, Quaternion.identity);
+                        }
+                        else // This could also spawn the biome type ground tile, could make this different
+                        {
+                            GameObject.Instantiate<GameObject>(groundTiles[rand.Next(amountOfGroundTiles)], spawnPosition, Quaternion.identity);
+                        }
+                    }
+
+                }
+            }
+        }
+    }
+}

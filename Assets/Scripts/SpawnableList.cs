189fb831751a88cb9dfda420060ff44d151e9819
akerholten <nikolai_aakk@hotmail.com>
2018-10-20 22:50:16 +0200

    Made it so tiles can regenerate vegetation or food

--- Assets/Scripts/SpawnableList.cs
+++ Assets/Scripts/SpawnableList.cs
@@ -1,8 +1,54 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Random = System.Random;
 
 public class SpawnableList : MonoBehaviour {
-
     public GameObject[] spawnables;
+    public bool canSpawnObjects;
+    public float timeSinceLastSpawn = 0.0f;
+    public int maxSpawnTimer = 200;
+    public int minSpawnTimer = 50;
+    private int spawnTimer;
+
+    private Random rand;
+
+    private void FixedUpdate()
+    {
+        if(canSpawnObjects == true)
+        {
+            timeSinceLastSpawn += Time.fixedDeltaTime;
+            if (timeSinceLastSpawn > spawnTimer)
+            {
+                timeSinceLastSpawn = 0.0f;
+                TryToSpawnObject();
+            }
+        }
+    }
+
+    private void TryToSpawnObject()
+    {
+        Vector3 rayCastPos = new Vector3(transform.position.x + 0.5f, transform.position.y, transform.position.z + 0.5f);
+        Vector3 spawnPos = new Vector3(transform.position.x + 0.5f, transform.position.y + 0.15f, transform.position.z + 0.5f);
+
+        if (!Physics.Raycast(rayCastPos, Vector3.up, maxDistance: 0.5f))
+        {
+            Instantiate(spawnables[rand.Next(spawnables.Length)], spawnPos, Quaternion.identity);
+            Debug.Log("Spawned!");
+            return;
+        }
+        Debug.Log("Didn't spawn!");
+    }
+
+    public void SetRandom(int seed)
+    {
+        rand = new Random(seed);
+
+        if (spawnables.Length >= 1)
+        {
+            canSpawnObjects = true;
+        }
+        spawnTimer = rand.Next(minSpawnTimer, maxSpawnTimer);
+    }
+
 }
